f4c7b43b8b82321f316df76467bea3393be797fa
Akhil08893 <akhilyerubandi143@gmail.com>
2024-03-09 18:55:55 +0530

    akhil

---
+++ love/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+
+def home(request):
+    if request.method=="POST":
+        first_name=request.POST['fname']
+        second_name=request.POST['lname']
+        first_name.lower()
+        second_name.lower()
+        s=list(second_name)
+        n=0
+        f=["f","l","a","m","e","s"]
+        for i in first_name:
+            if i in s:
+                s.remove(i)
+                n+=1
+        n=len(first_name)+len(second_name)-2*n
+        i=0
+        while len(f) > 1:
+            l = n % len(f) - 1
+            if l >= 0:
+                right=f[l+1:]
+                left=f[:l]
+                f=right+left
+            else:
+                f=f[:len(f)-1]
+        
+        if f[0] == 'f':
+            messages.success(request,"Friends😘")
+        elif f[0] == 'l':
+            messages.success(request,"Love💖")
+        elif f[0] == 'm':
+            messages.success(request,"Marriage💞")
+        elif f[0] == 'a':
+            messages.success(request,"Affection🥰")
+        elif f[0] == 'e':
+            messages.success(request,"Enemies😡")
+        else:
+            messages.success(request,"Siblings👥")
+        return redirect('home')
+    else:
+        return render(request,'flames.html')
+
